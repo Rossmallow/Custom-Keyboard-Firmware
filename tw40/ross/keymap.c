7286c49874133e0ac7931d9509fd1b9b880dd6d8
Ross Nelson <rossn430@gmail.com>
2021-05-23 21:26:39 -0500

    Swap layers 3 and 4

--- tw40/ross/keymap.c
+++ tw40/ross/keymap.c
@@ -73,25 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
-/* 3: Mouse/Numbers/Media
-┌───────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
-│ RESET │Ms 4 │Ms 2 │Ms Up│Ms 1 │MsWhU│PgUp │Home │ Up  │ End │ Ins │█████│█████│ Del   │
-├───────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴───────┤
-│Ms 3    │Ms 5 │MsLft│MsDwn│MsRgh│MsWhD│PgDn │Left │Down │Right│ Del │█████│Play        │
-├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴────────────┤
-│██████████│MsWhL│MsAc0│MsAc1│MsAc2│MsWhR│█████│PrScn│ScLck│Pause│█████│Mute            │
-├──────┬───┴──┬──┴───┬─┴────┬┴─────┴─────┼─────┴┬────┴────┬┴────┬┴────┬┴────┬─────┬─────┤
-│NLock │██████│██████│██████│ Backspace  │Space │█████████│Prev │VolUp│VolDn│Next │Stop │
-└──────┴──────┴──────┴──────┴────────────┴──────┴─────────┴─────┴─────┴─────┴─────┴─────┘
-*/
-[3] = LAYOUT_all(
-    RESET,   KC_BTN4, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_INS, KC_NO,   KC_NO,   KC_DEL,
-    KC_BTN3, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, KC_NO,            KC_MPLY,
-    KC_NO,   KC_WH_L, KC_ACL0, KC_ACL1, KC_ACL2, KC_WH_R, KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS,  KC_NO,           KC_MUTE, KC_MUTE,
-    KC_NLCK, KC_NO,   KC_NO,   KC_NO,   KC_TRNS,     KC_TRNS,      KC_NO,            KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, KC_MSTP
-),
-
-/* 4: Function/Numbers/Symbols
+/* 3: Function/Numbers/Symbols
 ┌───────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
 │ `     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ \     │
 ├───────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴───────┤
@@ -102,10 +84,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 │ Ctrl │ GUI  │ Alt  │██████│ Backspace  │Space │█████████│Left │ Up  │Down │Right│█████│
 └──────┴──────┴──────┴──────┴────────────┴──────┴─────────┴─────┴─────┴─────┴─────┴─────┘
 */
-[4] = LAYOUT_all(
+[3] = LAYOUT_all(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSLS,
     KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,
     KC_LPRN, KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_PLUS,          KC_RPRN, KC_RPRN,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS,     KC_TRNS,     KC_NO,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO
+),
+/* 4: Mouse/Numbers/Media
+┌───────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┐
+│ RESET │Ms 4 │Ms 2 │Ms Up│Ms 1 │MsWhU│PgUp │Home │ Up  │ End │ Ins │█████│█████│ Del   │
+├───────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴───────┤
+│Ms 3    │Ms 5 │MsLft│MsDwn│MsRgh│MsWhD│PgDn │Left │Down │Right│ Del │█████│Play        │
+├────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴────────────┤
+│██████████│MsWhL│MsAc0│MsAc1│MsAc2│MsWhR│█████│PrScn│ScLck│Pause│█████│Mute            │
+├──────┬───┴──┬──┴───┬─┴────┬┴─────┴─────┼─────┴┬────┴────┬┴────┬┴────┬┴────┬─────┬─────┤
+│NLock │██████│██████│██████│ Backspace  │Space │█████████│Prev │VolUp│VolDn│Next │Stop │
+└──────┴──────┴──────┴──────┴────────────┴──────┴─────────┴─────┴─────┴─────┴─────┴─────┘
+*/
+[4] = LAYOUT_all(
+    RESET,   KC_BTN4, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_INS, KC_NO,   KC_NO,   KC_DEL,
+    KC_BTN3, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, KC_NO,            KC_MPLY,
+    KC_NO,   KC_WH_L, KC_ACL0, KC_ACL1, KC_ACL2, KC_WH_R, KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS,  KC_NO,           KC_MUTE, KC_MUTE,
+    KC_NLCK, KC_NO,   KC_NO,   KC_NO,   KC_TRNS,     KC_TRNS,      KC_NO,            KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, KC_MSTP
 )
 };
