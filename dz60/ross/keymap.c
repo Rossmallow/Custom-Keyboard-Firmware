d8501f2fcb54e84ef8b34c341f086ba00b7c7c66
Ross Nelson <rossn430@gmail.com>
2021-05-31 18:39:20 -0500

    Add layers

--- dz60/ross/keymap.c
+++ dz60/ross/keymap.c
@@ -16,41 +16,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 └──────┴──────┴──────┴────────────────────────────────────┴─────┴─────┴─────┴─────┴─────┘
 */
 
-  LAYOUT_all(
-      KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
-
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-
-      MO(LAYER_1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-
-      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(LAYER_2),
-
-      KC_LCTL, KC_LGUI, KC_LALT, MO(LAYER_1),  MO(LAYER_1), KC_SPC, KC_RALT, MO(LAYER_1), KC_LEFT, KC_DOWN, KC_RIGHT
-      ),
+[0] = LAYOUT_all(
+    KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  ______,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, ______,  KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   MO(1),
+    KC_LCTL, KC_LGUI, KC_LALT,          ______,  ______, KC_SPC,            KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT
+),
 
 /* 1: Function Layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────────┐
-│ Esc │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  -  │  =  │ Bkspace │
+│  `  │  F1 │  F2 │  F3 │  F4 │  F5 │  F6 │  F7 │  F8 │  F9 │ F10 │ F11 │ F12 │ Delete  │
 ├─────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───────┤
-│ Tab   │  Q  │  W  │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │  Ent  │
+│ Tab   │Prev │Play │Stop │Next │  T  │  Y  │  U  │  I  │  O  │RgbNx│RgbUp│ClrUp│ RgbTg │
 ├───────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐      │
-│ Caps   │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │  '  │  \  │      │
+│ Caps   │VolDn│VolUp│Mute │  F  │  G  │  H  │  J  │  K  │  L  │RgbPv│RgbDn│ClrDn│      │
 ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬─────┤
-│ Shift   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │Shift│ Up  │Lyr 1│
+│ Shift   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │Shift│PgUp │█████│
 ├──────┬──┴───┬─┴────┬┴─────┴─────┴─────┴─────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-│ Ctrl │ GUI  │ Alt  │                Space               │ Alt │ GUI │Left │Down │Right│
+│ Ctrl │ GUI  │ Alt  │                Space               │ Alt │ GUI │Home │PgDn │ End │
 └──────┴──────┴──────┴────────────────────────────────────┴─────┴─────┴─────┴─────┴─────┘
 */
 
-  LAYOUT_all(
-      KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, KC_DEL,
-
-      ______, KC_MPRV, KC_MPLY, KC_MNXT, ______, ______, ______, ______, KC_UP, ______, ______, ______, ______, ______,
-
-      ______, ______, ______, ______, ______, ______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, ______, ______,
-
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLU, ______,
-
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLD, KC_MUTE
-      ),
+[1] = LAYOUT_all(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  ______,  KC_DEL,
+    KC_TRNS, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_TOG,
+    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_VAD, RGB_HUD,
+    KC_TRNS, ______,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,                ______,  ______, KC_SPC,                        KC_TRNS,  KC_TRNS, KC_HOME, KC_PGDN, KC_END
+),
 };
