ff0995d948f47ed66d32880cbe283710cc229ae3
Ross Nelson <rossn430@gmail.com>
2021-08-03 22:24:03 -0500

    Fix layers

--- v4n4g0n/ross/keymap.c
+++ v4n4g0n/ross/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
     CAD,     LOCK,    KC_MPLY,     KC_MUTE,     KC_VOLD,     KC_VOLU,
     KC_ESC,  KC_Q,    KC_W,        KC_E,        KC_R,        KC_T,    KC_Y,   KC_U,        KC_I,        KC_O,        KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    LT(5, KC_S), LT(4, KC_D), LT(3, KC_F), KC_G,    KC_H,   LT(3, KC_J), LT(4, KC_K), LT(5, KC_L), KC_QUOT, KC_ENT,
+    KC_TAB,  KC_A,    LT(4, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G,    KC_H,   LT(2, KC_J), LT(3, KC_K), LT(4, KC_L), KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,        KC_C,        KC_V,        KC_B,    KC_N,   KC_M,        KC_COMM,     KC_DOT,      KC_SLSH, TG(1),
     KC_LCTL, KC_LGUI, KC_LALT,                KC_NO,  KC_NO,  RCTL_T(KC_SPC), KC_NO,                    KC_LEFT,     KC_DOWN, KC_RIGHT
 ),
