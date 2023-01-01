87793b3da60409eb384143e22d4c8353538d697f
Ross Nelson <rossn430@gmail.com>
2023-01-01 13:03:53 -0600

    Update matrix layout

--- 40rmie/ross/config.h
+++ 40rmie/ross/config.h
@@ -4,18 +4,23 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-// #define DESCRIPTION     The 40rmie v2 keyboard by KuroTheCrazy
+//#define VENDOR_ID       0xFEED
+//#define PRODUCT_ID      0x6060
+//#define DEVICE_VER      0x0001
+//#define MANUFACTURER    KuroTheCrazy
+//#define PRODUCT         40rmie
+//#define DESCRIPTION     The 40rmie v2 keyboard by KuroTheCrazy
 
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4 }
-#define MATRIX_COL_PINS { B5, B6, B7, C6, C7, D0, D1, D2, D3, D4, D5, D6, D7, F0, F1 }
+//#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4 }
+//#define MATRIX_COL_PINS { B5, B6, B7, C6, C7, D0, D1, D2, D3, D4, D5, D6, D7, F0, F1 }
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+//#define DIODE_DIRECTION COL2ROW
 
 /* number of backlight levels */
 
@@ -37,7 +42,11 @@
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
+/* prevent stuck modifiers */
+//#define PREVENT_STUCK_MODIFIERS
+
 #ifdef RGB_DI_PIN
+//#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 0
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
