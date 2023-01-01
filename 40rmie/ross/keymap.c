7a030841a80f2452b42d6baa1953fffd50fd2134
Ross Nelson <rossn430@gmail.com>
2022-12-31 18:58:32 -0600

    Begin custom keymapping for 40rmie

--- 40rmie/ross/keymap.c
+++ 40rmie/ross/keymap.c
@@ -18,12 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 └─────┴─────┴─────┴───────────────────────────────────────┴─────┴─────┴─────┘ └─────┴─────┴─────┘
 */
 [0] = LAYOUT(
-    CAD,     LGUI(KC_L), KC_MPLY,     KC_MUTE,     KC_VOLD,     KC_VOLU,
-    KC_ESC,  KC_Q,       KC_W,        KC_E,        KC_R,        KC_T,    KC_Y,   KC_U,        KC_I,        KC_O,        KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,       LT(4, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G,    KC_H,   LT(2, KC_J), LT(3, KC_K), LT(4, KC_L), KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z,       KC_X,        KC_C,        KC_V,        KC_B,    KC_N,   KC_M,        KC_COMM,     KC_DOT,      KC_SLSH, TG(1),
-    KC_LCTL, KC_LGUI,    KC_LALT,                KC_NO,  KC_NO,  RCTL_T(KC_SPC), KC_NO,                    KC_LEFT,     KC_DOWN, KC_RIGHT
-),
+		   KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                CAD, LGUI(KC_L), KC_MPLY, KC_MUTE,      KC_INS,  KC_HOME, KC_PGUP, 
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,   KC_DEL,  KC_END,  KC_PGDN, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_UP, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_LEFT, KC_DOWN, KC_RIGHT),
 
 /* 1: Gaming Layer
     ┌─────┬─────┬─────┬─────┬─────┬─────┐
