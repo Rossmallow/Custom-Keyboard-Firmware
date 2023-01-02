8f2938eba0054cee7d8dc78fc8746d60d8259666
Ross Nelson <rossn430@gmail.com>
2023-01-01 23:48:00 -0600

    Final (for now) firmware

--- 40rmie/keymaps/ross/keymap.c
+++ 40rmie/keymaps/ross/keymap.c
@@ -18,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 └─────┴─────┴─────┴───────────────────────────────────────┴─────┴─────┴─────┘ └─────┴─────┴─────┘
 */
 [0] = LAYOUT(
-    		    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                                             CAD, LGUI(KC_L), KC_MPLY, KC_MUTE,                     KC_INS,  KC_HOME, KC_PGUP,
-    		 KC_ESC,  KC_Q,    KC_W,        KC_E,        KC_R,        KC_T,           KC_Y,   KC_U,        KC_I,        KC_O,        KC_P,    KC_BSPC,                 KC_DEL,  KC_END,  KC_PGDN,
-    		 KC_TAB,  KC_A,    LT(4, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G,           KC_H,   LT(2, KC_J), LT(3, KC_K), LT(4, KC_L), KC_QUOT, KC_ENT,  KC_ENT, KC_ENT,
-    KC_LSFT, KC_LSFT, KC_Z,    KC_X,        KC_C,        KC_V,        KC_B,           KC_N,   KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,                                   KC_UP,
-    		 KC_LCTL, KC_LGUI, KC_LALT,     KC_SPC,      KC_SPC,      RCTL_T(KC_SPC), KC_SPC, KC_SPC       KC_RALT,     TG(1),       KC_RCTL,                          KC_LEFT, KC_DOWN, KC_RIGHT
+    		    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                                             CAD, LGUI(KC_L), KC_MPLY, KC_MUTE,                     KC_INS,  KC_HOME, KC_PGUP, //13
+    		 KC_ESC,  KC_Q,    KC_W,        KC_E,        KC_R,        KC_T,           KC_Y,   KC_U,        KC_I,        KC_O,        KC_P,    KC_BSPC,                 KC_DEL,  KC_END,  KC_PGDN, //15
+    		 KC_TAB,  KC_A,    LT(4, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G,           KC_H,   LT(2, KC_J), LT(3, KC_K), LT(4, KC_L), KC_QUOT, KC_ENT,  KC_ENT, KC_ENT, //14
+    KC_LSFT, KC_LSFT, KC_Z,    KC_X,        KC_C,        KC_V,        KC_B,           KC_N,   KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,                                   KC_UP, //13
+    		 KC_LCTL, KC_LGUI, KC_LALT,     KC_SPC,      KC_SPC,      RCTL_T(KC_SPC), KC_SPC, KC_SPC,      KC_RALT,     TO(1),       KC_RCTL,                          KC_LEFT, KC_DOWN, KC_RIGHT //14
 ),
 
 /* 1: Gaming Layer
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,
     		 KC_TRNS, KC_TRNS, KC_S,    KC_D,    KC_F,    KC_TRNS, KC_TRNS, KC_J,    KC_K,    KC_L,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS,
-    		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, TG(0),   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS
+    		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, TO(0),   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* 2: Numbers/Navigation Layer
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT(
     		    KC_7, KC_8, KC_9, KC_0, KC_LPRN, KC_RPRN,                       KC_VOLU, KC_VOLD, KC_MPRV, KC_MNXT,                  KC_TRNS, KC_TRNS, KC_TRNS,
     		 KC_GRV,  KC_LBRC, KC_P9, KC_P8,   KC_P7,   KC_RBRC, KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_INS,  KC_DEL,               KC_TRNS, KC_TRNS, KC_TRNS,
-    		 KC_LNUM, KC_SCLN, KC_P6, KC_P5,   KC_P4,   KC_EQL,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_NO,  KC_NO, KC_NO,
+    		 KC_NUM,  KC_SCLN, KC_P6, KC_P5,   KC_P4,   KC_EQL,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_NO,  KC_NO, KC_NO,
     KC_TRNS, KC_TRNS, KC_MINS, KC_P3, KC_P2,   KC_P1,   KC_BSLS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                                  KC_TRNS,
     		 KC_LALT, KC_PDOT, KC_P0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,    KC_NO,                         KC_TRNS, KC_TRNS, KC_TRNS
 ),
